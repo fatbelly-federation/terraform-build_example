b2f71f8039851ecbf1b3b8959a1ae8c1ae2cab19
GPanula <github@kablah.com>
2018-10-21 13:27:03 -0500

    update to enforce s3 encryption

--- aws/pre-init/init.tf
+++ aws/pre-init/init.tf
@@ -5,7 +5,7 @@
 
 module "pre-init" {
   #source  = "../../../modules//tfm_pre-init"
-  source = "github.com/fatbelly-federation/tfm_pre-init?ref=v1.0.1"
+  source = "github.com/fatbelly-federation/tfm_pre-init?ref=v1.1.0"
 
   # we read in variable values from ../terraform.tfvars
   # and use them to set values for the variables the modules is expecting
@@ -24,8 +24,9 @@ module "pre-init" {
   # can have generic values that highlight when a build is missing them
   tags = {
     "tfm_module"          = "github.com/fatbelly-federation/tfm_pre-init"
-    "tfm_module_version"  = "v1.0.1"
+    "tfm_module_version"  = "v1.0.2"
     "build_date"          = "2018-Aug-28"
+    "modified_date"       = "2018-Oct-20"
     "build_path"          = "terraform-build_example/aws/pre-init"
     "billing_account"     = "aws_fb_12345"
     "build_url"           = "https://github.com/fatbelly-federation/terraform-build_example"
